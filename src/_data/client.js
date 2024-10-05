407fa84f67823ab445d45e94eb04199d35527734
Myles (Work) <myles@unicard.com.au>
2024-10-05 16:23:37 +1000

    start swapping out text

--- src/_data/client.js
+++ src/_data/client.js
@@ -1,21 +1,21 @@
 module.exports = {
-    "name": "Code Stitch Web Designs",
-    "email": "help@codestitch.app",
-    "phoneForTel": "555-779-4407",
-    "phoneFormatted": "(555) 779-4407",
-    "address": {
-        "lineOne": "First Address Line",
-        "lineTwo": "Second Address Line",
-        "city": "Denver",
-        "state": "CO",
-        "zip": "80206",
-        "country": "US",
-        "mapLink": "https://maps.app.goo.gl/TEdS5KoLC9ZcULuQ6"
-    },
-    "socials": {
-        "facebook": "https://www.facebook.com/",
-        "instagram": "https://www.instagram.com/",
-    },
-    //! Make sure you include the file protocol (e.g. https://) and that NO TRAILING SLASH is included
-    "domain": "https://www.example.com"
+  "name": "Baulkham Hills Web Designs",
+  "email": "mylesjpritchett@gmail.com",
+  "phoneForTel": "0412629827",
+  "phoneFormatted": "(+61) 04 1262 9827",
+  "address": {
+    "lineOne": "Unit 43 11 Glenvale Avenue",
+    "lineTwo": "",
+    "city": "Baulkham Hills",
+    "state": "NSW",
+    "zip": "2768",
+    "country": "AUS",
+    "mapLink": "https://maps.app.goo.gl/SgMChnmSHsR7zgAQ8"
+  },
+  "socials": {
+    "facebook": "https://www.facebook.com/",
+    "instagram": "https://www.instagram.com/",
+  },
+  //! Make sure you include the file protocol (e.g. https://) and that NO TRAILING SLASH is included
+  "domain": "https://baulkham-hills-web-design.netlify.app"
 };
